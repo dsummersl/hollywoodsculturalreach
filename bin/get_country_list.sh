f7977acd5451065c017ad2f8752ddef3ccade852
Dane Summers <dsummersl@yahoo.com>
2012-01-17 13:05:17 -0500

    updated the data - we had lots of non-real foreign data. Fixed that

--- bin/get_country_list.sh
+++ bin/get_country_list.sh
@@ -2,7 +2,7 @@
 
 # get a list of all the countries that are on the movie mojo site.
 
-year=2007
+year=$1
 firstLine=yes
 while read line
 do
@@ -16,22 +16,29 @@ do
     baseurl='http://boxofficemojo.com'
     echo "$baseurl/intl/$key/yearly/"
     curl "$baseurl/intl/$key/yearly/?yr=$year" > list.html
-    # the number of pages we have to get:
-    tidy list.html | xml fo -H | xml sel -T -t -m '//a[contains(@href,"pagenum")]' -v @href -n | sort | uniq > additionalpages.txt
-    # http://boxofficemojo.com/intl/france/yearly/?yr=2011
-    # http://boxofficemojo.com/intl/france/yearly/?yr=2011&sort=gross&order=DESC&pagenum=3&p=.htm
 
-    mkdir -p data/$year/$key
+    # check that we actually got the $year back
+    gotback=`tidy list.html | xml fo -H | xml sel -T -t -m '//head/title' -v "str:split(.)[1]"`
 
-    # NOTE: turns out OS X sed is not quite the same as GNU sed. Working out better now that I got gnu sed in...
-    # clean up with tidy (but tidy introduces extra carrage returns | select the table rows of data and format as CSV | then use sed to fix the extra carriage returns | then use sed to fix up the first line
-    tidy list.html | xml fo -H | xml sel -T -t -m '//table/tr' -i 'count(./td)=5' -v "concat('\"',str:replace(./td[2],'&#10;',''),'\",\"',./td[3],'\",\"',./td[4],'\",\"',./td[5],'\"')" -n | gsed -e '/^[^,]*$/ { N; s/\n/ / }' -e ':begin 1,4 { N; s/\n//; b begin }' > data/$year/$key/data.csv
+    if [ "$gotback" -eq "$year" ]
+    then
+      # the number of pages we have to get:
+      tidy list.html | xml fo -H | xml sel -T -t -m '//a[contains(@href,"pagenum")]' -v @href -n | sort | uniq > additionalpages.txt
+      # http://boxofficemojo.com/intl/france/yearly/?yr=2011
+      # http://boxofficemojo.com/intl/france/yearly/?yr=2011&sort=gross&order=DESC&pagenum=3&p=.htm
 
-    while read additionalpage
-    do
-      curl "$baseurl/$additionalpage" > list.html
-      tidy list.html | xml fo -H | xml sel -T -t -m '//table/tr' -i 'count(./td)=5' -v "concat('\"',str:replace(./td[2],'&#10;',''),'\",\"',./td[3],'\",\"',./td[4],'\",\"',./td[5],'\"')" -n | gsed -e '/^[^,]*$/ { N; s/\n/ / }' -e ':begin 1,4 { N; s/\n//; b begin }' -e '1,1 d' >> data/$year/$key/data.csv
-    done < additionalpages.txt
+      mkdir -p data/$key
+
+      # NOTE: turns out OS X sed is not quite the same as GNU sed. Working out better now that I got gnu sed in...
+      # clean up with tidy (but tidy introduces extra carrage returns | select the table rows of data and format as CSV | then use sed to fix the extra carriage returns | then use sed to fix up the first line
+      tidy list.html | xml fo -H | xml sel -T -t -m '//table/tr' -i 'count(./td)=5' -v "concat('\"',str:replace(./td[2],'&#10;',''),'\",\"',./td[3],'\",\"',./td[4],'\",\"',./td[5],'\"')" -n | gsed -e '/^[^,]*$/ { N; s/\n/ / }' -e ':begin 1,4 { N; s/\n//; b begin }' > data/$key/$year.csv
+
+      while read additionalpage
+      do
+        curl "$baseurl/$additionalpage" > list.html
+        tidy list.html | xml fo -H | xml sel -T -t -m '//table/tr' -i 'count(./td)=5' -v "concat('\"',str:replace(./td[2],'&#10;',''),'\",\"',./td[3],'\",\"',./td[4],'\",\"',./td[5],'\"')" -n | gsed -e '/^[^,]*$/ { N; s/\n/ / }' -e ':begin 1,4 { N; s/\n//; b begin }' -e '1,1 d' >> data/$key/$year.csv
+      done < additionalpages.txt
+    fi
   fi
 done < data/countries.csv
 #done < c.txt
